0d3da8cd01d03b670d8ecb769f7a4e0183bd07b4
kirill-mktsight <kdubinin@marketsight.com>
2020-05-18 11:15:37 +0300

    all

---
+++ pages/labels/sets/set-002-spring.vue
@@ -0,0 +1,453 @@
+<template>
+  <section class="offer">
+    <el-row type="flex" class="offer__logo">
+      <el-col :span="6" class="left">
+        <h3>PESCARY</h3>
+        <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" height="10mm" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+        viewBox="0 0 13891 668"
+        xmlns:xlink="http://www.w3.org/1999/xlink">
+        <g>
+          <path class="fil0" d="M3843 455c-77,55 -190,114 -278,144 -151,52 -311,69 -470,69 -167,0 -336,-18 -494,-73 -95,-33 -184,-79 -263,-139 -13,-10 -30,-10 -43,0 -85,61 -178,110 -278,144 -152,52 -310,69 -470,69 -167,0 -336,-18 -494,-73 -96,-33 -184,-79 -264,-139 -13,-10 -30,-10 -43,0 -85,61 -178,110 -278,144 -45,15 -91,28 -137,37 -10,2 -20,0 -29,-6 -101,-71 -197,-148 -289,-229 -11,-10 -15,-24 -11,-38 4,-14 16,-24 31,-25 102,-12 250,-31 326,-57 114,-39 210,-102 295,-187l72 -72c14,-14 32,-21 53,-21 20,1 38,9 51,24l68 77c73,83 161,140 266,176 121,42 256,54 383,54 122,0 246,-11 361,-51 114,-39 210,-102 294,-187l72 -72c15,-14 32,-21 53,-21 20,1 38,9 51,24l68 77c74,83 161,140 266,176 121,42 256,54 383,54 38,0 76,-1 114,-4 89,-12 189,-27 247,-47 112,-38 209,-102 294,-187l72 -72c8,-8 17,-14 27,-17l1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 1 0 1 0 1 0 0 0 1 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 2 0 0 0c16,2 31,10 42,23l68 77c73,83 161,140 266,176 47,16 95,28 145,36 71,11 164,18 238,18 122,0 246,-11 361,-51 114,-39 210,-102 294,-187l72 -72c15,-14 32,-21 53,-21 20,1 38,9 51,24l68 77c74,83 161,140 266,176 121,42 256,54 383,54 38,0 76,-1 114,-4 89,-12 189,-27 247,-47 112,-38 210,-103 294,-187l72 -72c15,-15 32,-21 51,-21 22,0 39,8 53,24l68 77c73,83 161,140 266,176 47,16 95,28 145,36 71,11 164,18 238,18 122,0 246,-11 361,-51 114,-39 210,-102 294,-187l72 -72c15,-14 32,-21 53,-21 20,1 38,9 51,24l68 77c74,83 161,140 266,176 121,42 256,54 383,54 38,0 76,-1 114,-4 89,-12 189,-27 247,-47 112,-38 209,-102 294,-187l72 -72c8,-8 17,-14 27,-17l1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 1 0 1 0 1 0 0 0 1 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 2 0 0 0c16,2 31,10 42,23l68 77c73,83 161,140 266,176 47,16 95,28 145,36 71,11 164,18 238,18 122,0 246,-11 361,-51 114,-39 210,-102 294,-187l72 -72c15,-14 32,-21 53,-21 20,1 38,9 51,24l68 77c74,83 161,140 266,176 121,42 256,54 383,54 122,0 246,-11 361,-51 114,-39 210,-102 294,-187l72 -72c14,-14 32,-21 53,-21 20,1 38,9 51,24l68 77c73,83 161,140 266,176 109,38 199,45 302,50 15,1 28,10 33,24 5,14 1,30 -10,40 -92,82 -188,159 -289,231 -9,6 -19,8 -30,6 -39,-9 -78,-20 -116,-34 -96,-33 -184,-79 -264,-139 -13,-10 -30,-10 -43,0 -85,61 -178,109 -279,144 -152,52 -310,69 -470,69 -167,0 -336,-18 -494,-73 -95,-33 -184,-79 -263,-139 -13,-10 -30,-10 -43,0 -85,61 -178,110 -278,144 -152,52 -310,69 -470,69 -164,0 -337,-18 -494,-73 -96,-33 -184,-79 -264,-139 -13,-10 -30,-10 -43,0 -77,55 -190,114 -278,144 -151,52 -311,69 -470,69 -167,0 -336,-18 -494,-73 -95,-33 -184,-79 -263,-139 -13,-10 -30,-10 -43,0 -85,61 -178,110 -278,144 -152,52 -310,69 -470,69 -164,0 -337,-18 -494,-73 -96,-33 -184,-79 -264,-139 -13,-10 -30,-10 -43,0 -86,61 -178,109 -278,144 -151,52 -311,69 -470,69 -167,0 -336,-18 -494,-73 -95,-33 -184,-79 -263,-139 -13,-10 -30,-10 -43,0 -85,61 -178,110 -278,144 -152,52 -310,69 -470,69 -164,0 -337,-18 -494,-73 -96,-33 -184,-79 -264,-139 -13,-10 -30,-10 -43,0z"/>
+        </g>
+        </svg>
+      </el-col>
+      <el-col :span="12" class="center">
+        <h3>Сет #002. ВЕСНА.<br/> Первый раз на платнике. Фидер.</h3>
+      </el-col>
+      <el-col :span="6" class="right">
+        <h3>pescary.com</h3>
+        <h3>sales@prokorm.com</h3>
+        <h3>8 800 550 28 27</h3>
+      </el-col>
+    </el-row>
+    <div class="offer__section">
+      <div class="set-001-spring"/>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+
+    let red = 'rgb(202, 87, 87)';
+    let yellow = 'rgb(251, 212, 45)';
+    let orange = 'rgb(245, 155, 44)';
+    let white = 'rgb(241, 241, 241)';
+    let brown = 'rgb(160, 126, 126)';
+    let green = '#467d46';
+    const fishGranule = [
+      {
+        name: 'Strawberry Cream',
+        aroma: 'Клубника со сливками',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Honey',
+        aroma: 'Мед',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Tutti Frutti',
+        aroma: 'Тутти-Фрутти',
+        color: 'Оранжевый',
+        background: orange
+      },
+      {
+        name: 'Plum',
+        aroma: 'Слива',
+        color: 'Оранжевый',
+        background: orange
+      },
+      {
+        name: 'Pineapple',
+        aroma: 'Ананас',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Tangerine',
+        aroma: 'Мандарин',
+        color: 'Оранжевый',
+        background: orange
+      },
+      {
+        name: 'Mulberry',
+        aroma: 'Шелковица',
+        color: 'Оранжевый',
+        background: orange
+      },
+      {
+        name: 'Banana',
+        aroma: 'Банан',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Tiger Nut',
+        aroma: 'Тигровый орех',
+        color: 'Белый',
+        background: white
+      },
+      {
+        name: 'Hot Spices',
+        aroma: 'Острые специи',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Garlic',
+        aroma: 'Чеснок',
+        color: 'Белый',
+        background: white
+      },
+      {
+        name: 'Monster Crab',
+        aroma: 'Монстр Краб',
+        color: 'Коричневый',
+        background: brown
+      },
+      {
+        name: 'White Fish',
+        aroma: 'Белая рыба',
+        color: 'Коричневый',
+        background: brown
+      },
+      {
+        name: 'Squid',
+        aroma: 'Кальмар',
+        color: 'Коричневый',
+        background: brown
+      }
+    ];
+
+    const lureBream = [
+      {
+        name: 'Sweet Biscuit ',
+        aroma: 'Сладкое печенье',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Melon',
+        aroma: 'Дыня',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Anise',
+        aroma: 'Анис',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Vanilla ',
+        aroma: 'Ваниль',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Milk Chocolate ',
+        aroma: 'Молочный шоколад',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Caramel',
+        aroma: 'Карамель',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Provencal Spices',
+        aroma: 'Прованские специи',
+        color: 'Желтый',
+        background: red
+      },
+      {
+        name: 'Peanut',
+        aroma: 'Арахис',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Cinnamon ',
+        aroma: 'Корица',
+        color: 'Красный',
+        background: red
+      }
+    ];
+
+    const aroma = [{
+      name: 'Strawberry Cream',
+      aroma: 'Клубника со сливками',
+      color: 'Красный',
+      background: red
+    }, {
+      name: 'Honey',
+      aroma: 'Мед',
+      color: 'Желтый',
+      background: yellow
+    }, {
+      name: 'Tutti Frutti',
+      aroma: 'Тутти-Фрутти',
+      color: 'Красный',
+      background: red
+    }, {
+      name: 'Plum',
+      aroma: 'Слива',
+      color: 'Красный',
+      background: red
+    }, {
+      name: 'Pineapple',
+      aroma: 'Ананас',
+      color: 'Желтый',
+      background: yellow
+    }, {
+      name: 'Tangerine',
+      aroma: 'Мандарин',
+      color: 'Желтый',
+      background: yellow
+    }, {
+      name: 'Mulberry',
+      aroma: 'Шелковица',
+      color: 'Красный',
+      background: red
+    }, {
+      name: 'Banana',
+      aroma: 'Банан',
+      color: 'Желтый',
+      background: yellow
+    }, {
+      name: 'Tiger Nut',
+      aroma: 'Тигровый орех',
+      color: 'Желтый',
+      background: yellow
+    }, {
+      name: 'Hot Spices',
+      aroma: 'Острые специи',
+      color: 'Красный',
+      background: red
+    }, {
+      name: 'Garlic',
+      aroma: 'Чеснок',
+      color: 'Желтый',
+      background: yellow
+    }, {
+      name: 'Monster Crab',
+      aroma: 'Монстр Краб',
+      color: 'Коричневый',
+      background: brown
+    }, {
+      name: 'White Fish',
+      aroma: 'Белая рыба',
+      color: 'Коричневый',
+      background: brown
+    }, {
+      name: 'Squid',
+      aroma: 'Кальмар',
+      color: 'Коричневый',
+      background: brown
+    }]
+
+    const lureCarp = [
+      {
+        name: 'Flat Feeder',
+        aroma: 'Флэт фидер',
+        color: 'Черный',
+        background: '#555'
+      },
+      {
+        name: 'Strawberry Cream ',
+        aroma: 'Клубника со сливками',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Honey',
+        aroma: 'Мед',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Tutti Frutti',
+        aroma: 'Тутти-Фрутти',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Plum',
+        aroma: 'Слива',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Pineapple',
+        aroma: 'Ананас',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Tangerine',
+        aroma: 'Мандарин',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Mulberry',
+        aroma: 'Шелковица',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Banana',
+        aroma: 'Банан',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Tiger Nut',
+        aroma: 'Тигровый орех',
+        color: 'Желтый',
+        background: yellow
+      },
+      {
+        name: 'Hot Spices',
+        aroma: 'Острые специи',
+        color: 'Красный',
+        background: red
+      },
+      {
+        name: 'Garlic',
+        aroma: 'Чеснок',
+        color: 'Желтый',
+        background: yellow
+      }
+    ];
+    return {
+      fishGranule: fishGranule,
+      lureBream: lureBream,
+      lureCarp: lureCarp,
+      aroma: aroma
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @page {
+    size: 210mm 218mm; // set appropriately
+    margin: 0;
+  }
+  @media print {
+    html, body {
+      width: 210mm; // set appropriately
+      height: 218mm; // set appropriately
+      margin: 0;
+    }
+  }
+  .set-001-spring {
+    width: 100%;
+    height: 280mm;
+    background: url('/images/sets/set-002-spring.jpg');
+    background-size: 300mm;
+    background-repeat: no-repeat;
+    background-position: -140px 0;
+  }
+  .offer {
+    margin: 0;
+    padding: 0;
+    width: 210mm;
+    height: 220mm;
+    font-weight: 900;
+    margin: 0 auto;
+    font-family: 'Montserrat', cursive;
+    font-size: 12px;
+    font-weight: 100;
+    .el-row{
+      padding: 2px 0;
+    }
+    .el-col {
+      padding: 2px;
+    }
+    h1 {
+      font-size: 18px;
+      margin: 30px 0 0 0;
+      border-bottom: 1px solid #999;
+      padding-bottom: 6px;;
+      span {
+        font-weight: 100;
+        margin-left: 20px;
+        font-size: 14px;
+      }
+    }
+
+    &__description {
+      line-height: 16px;
+      margin: 10px 0;
+    }
+    &__header {
+      padding: 10px 0 !important;
+      text-transform: uppercase;
+      color: #1f2e35;
+    }
+    &__price {
+      text-align: right;
+    }
+
+    &__color {
+      text-align: center;
+    }
+    &__sub {
+      margin: 40px 0 12px 0;
+      font-size: 16px;
+    }
+    &__composition {
+      margin: 6px 0;
+      text-align: justify;
+      font-size: 12px;
+      line-height: 20px;
+    }
+    &__section {
+    }
+    &__logo {
+      background: #1f2e35;
+      font-family: 'Core Rhino';
+      color: #f0f0f0;
+      font-size: 30px;
+      position: relative;
+      padding: 12px 12px !important;
+
+      svg {
+        fill: #f0f0f0;
+        height: 6px;
+        position: absolute;
+        left: 40px;
+        top: 54px;
+      }
+
+      .left {
+        padding-left: 12px;
+      }
+      .center {
+        text-align: center;
+        font-size: 16px;
+        font-family: 'Montserrat', cursive;
+        padding-top: 0px;
+      }
+      .right {
+        padding-right: 12px;
+        font-family: 'Montserrat', cursive;
+        font-size: 11px;
+        text-align: right;
+        font-weight: 100;
+      }
+    }
+  }
+
+</style>
+
