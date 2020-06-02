faccfb919c52bf8565a5d1cb375f332cd0371eb2
kirill-mktsight <kdubinin@marketsight.com>
2020-06-02 20:19:23 +0300

    master

--- components/MainHeader.vue
+++ components/MainHeader.vue
@@ -11,7 +11,7 @@
       <div class="index__header--navigator flex-1 block sm:flex ml-4 mt-4 text-center sm:text-center">
         <NuxtLink :class="link.className" class="block sm:inline text-sm mx-2 h-16 px-4 pt-5 transition duration-150 ease-in-out" :key="link.to" v-for="link in links" :to="link.to">{{link.label}}</NuxtLink>
       </div>
-      <div class="index__header--social flex mt-6">
+      <div class="index__header--social flex justify-center mb-4 sm:mb-0 mt-6">
         <a href="https://vk.com/public195411230" target="_blank" class="mx-2" title="PESCARY в VK">
           <svg class="" enable-background="new 0 0 24 24" height="512" viewBox="0 0 24 24" width="512" xmlns="http://www.w3.org/2000/svg"><path d="m19.915 13.028c-.388-.49-.277-.708 0-1.146.005-.005 3.208-4.431 3.538-5.932l.002-.001c.164-.547 0-.949-.793-.949h-2.624c-.668 0-.976.345-1.141.731 0 0-1.336 3.198-3.226 5.271-.61.599-.892.791-1.225.791-.164 0-.419-.192-.419-.739v-5.105c0-.656-.187-.949-.74-.949h-4.126c-.419 0-.668.306-.668.591 0 .622.945.765 1.043 2.515v3.797c0 .832-.151.985-.486.985-.892 0-3.057-3.211-4.34-6.886-.259-.713-.512-1.001-1.185-1.001h-2.625c-.749 0-.9.345-.9.731 0 .682.892 4.073 4.148 8.553 2.17 3.058 5.226 4.715 8.006 4.715 1.671 0 1.875-.368 1.875-1.001 0-2.922-.151-3.198.686-3.198.388 0 1.056.192 2.616 1.667 1.783 1.749 2.076 2.532 3.074 2.532h2.624c.748 0 1.127-.368.909-1.094-.499-1.527-3.871-4.668-4.023-4.878z"/></svg>
         </a>
